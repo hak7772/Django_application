9bbddb5bf9b4b62ebb1d7cd0020368bbd84929a8
hak7772 <soldatov.dmitriy123@gmail.com>
2025-10-11 15:02:53 +0700

    init

--- filemanager/files/apps.py
+++ filemanager/files/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FilesappConfig(AppConfig):
+class FilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'filesapp'
+    name = 'files'
